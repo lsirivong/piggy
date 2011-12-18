00fe3d46411b0f4e1a9be9ac38681c239fb44642
Lenny Sirivong <lsirivong@gmail.com>
2011-12-17 18:18:10 -0600

    Set focus to vendor when Edit link is clicked

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,5 @@ function submit_focus(formId) {
 
 function add_fields_to_edit_transaction(transaction_id, content) {
   $('#'+transaction_id).replaceWith(content);
+  $('#'+transaction_id + " .fields input").first().trigger('focus');
 }
