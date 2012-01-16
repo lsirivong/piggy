4ae3011d936f989d866ae5437479138c2863dd26
Lenny Sirivong <lsirivong@gmail.com>
2012-01-16 13:05:47 -0600

    On transaction create and update, check if the form has been submitted already using the submit table. if not, proceed.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -31,9 +31,21 @@ class TransactionsController < ApplicationController
   # POST /transactions.json
   def create
     @transaction = Transaction.new(params[:transaction])
+    
+    # Check if this form has been submitted already.
+    @submit
+    unless params[:time].nil?
+      form_time = Time.parse(params[:time])
+      @submit = Submit.find_by_time(form_time)
+    end
 
     respond_to do |format|
-      if @transaction.save
+      if @submit.nil? && @transaction.save
+        
+        # track that this form time has been successfully submitted
+        @submit = Submit.new(:time => form_time)
+        @submit.save!
+        
         format.html { redirect_to @transaction, notice: 'Transaction was successfully created.' }
         @envelope = @transaction.envelope
         format.js
@@ -49,9 +61,21 @@ class TransactionsController < ApplicationController
   # PUT /transactions/1.json
   def update
     @transaction = Transaction.find(params[:id])
+    
+    # Check if this form has been submitted already.
+    @submit
+    unless params[:time].nil?
+      form_time = Time.parse(params[:time])
+      @submit = Submit.find_by_time(form_time)
+    end
 
     respond_to do |format|
-      if @transaction.update_attributes!(params[:transaction])
+      if @submit.nil? && @transaction.update_attributes!(params[:transaction])
+        
+        # track that this form time has been successfully submitted
+        @submit = Submit.new(:time => form_time)
+        @submit.save!
+        
         format.html { redirect_to :root, notice: 'Transaction was successfully updated.' }
         format.js
         format.json { head :ok }
