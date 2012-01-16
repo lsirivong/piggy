3be7449b6ac7c3fb665186aa88dba71e02e94f44
Lenny Sirivong <lsirivong@gmail.com>
2012-01-16 14:11:58 -0600

    Move multiple submission code into a before filter only applied to js requests.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,8 @@
 class TransactionsController < ApplicationController
+  before_filter(:only => [:create, :update]) do |controller|
+    return prevent_multiple_submission if controller.request.format.js?
+  end
+  
   # GET /transactions/1
   # GET /transactions/1.json
   def show
@@ -31,23 +35,10 @@ class TransactionsController < ApplicationController
   # POST /transactions.json
   def create
     @transaction = Transaction.new(params[:transaction])
-    
-    # Check if this form has been submitted already.
-    @submit
-    unless params[:time].nil?
-      form_time = Time.parse(params[:time])
-      @submit = Submit.find_by_time(form_time)
-    end
 
     respond_to do |format|
-      if @submit.nil? && @transaction.save
-        
-        # track that this form time has been successfully submitted
-        @submit = Submit.new(:time => form_time)
-        @submit.save!
-        
+      if @transaction.save
         format.html { redirect_to @transaction, notice: 'Transaction was successfully created.' }
-        @envelope = @transaction.envelope
         format.js
         format.json { render json: @transaction, status: :created, location: @transaction }
       else
@@ -61,21 +52,9 @@ class TransactionsController < ApplicationController
   # PUT /transactions/1.json
   def update
     @transaction = Transaction.find(params[:id])
-    
-    # Check if this form has been submitted already.
-    @submit
-    unless params[:time].nil?
-      form_time = Time.parse(params[:time])
-      @submit = Submit.find_by_time(form_time)
-    end
 
     respond_to do |format|
-      if @submit.nil? && @transaction.update_attributes!(params[:transaction])
-        
-        # track that this form time has been successfully submitted
-        @submit = Submit.new(:time => form_time)
-        @submit.save!
-        
+      if update_attributes!(params[:transaction])
         format.html { redirect_to :root, notice: 'Transaction was successfully updated.' }
         format.js
         format.json { head :ok }
@@ -96,6 +75,23 @@ class TransactionsController < ApplicationController
       format.html { redirect_to :root }
       format.js
       format.json { head :ok }
+    end
+  end
+  
+  private
+  
+  def prevent_multiple_submission
+    unless params[:time].nil?
+      form_time = Time.parse(params[:time])
+      submission = Submit.find_by_time(form_time)
+    
+      if submission.nil?
+        submission = Submit.new(:time => form_time)
+        submission.save!
+      else
+        # respond with nothing, request is halted
+        head :no_content
+      end
     end
   end
 end
