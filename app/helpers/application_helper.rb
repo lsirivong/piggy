9e8e046112a0d6299a97f15f134b53f9f55d9e1f
Lenny Sirivong <lsirivong@gmail.com>
2012-01-16 11:10:56 -0600

    add methods to set the amount_over visibility, move animation snippet into a method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,53 +17,28 @@ module ApplicationHelper
   
   def recalculate_stats(transaction)
     envelope = transaction.envelope
-    animation_after = "
-    .animate(
-      { opacity: 1 },
-       350, function() {
-         // Animation complete.
-      }
-    )"
-    animate = "$('%s').animate(
-      { opacity: 0 },
-       350, function() {
-         $(this).html('%s').animate(
-           { opacity: 1 },
-           350, function() {
-             // Animation complete.
-           }
-        );
-      }
-    );"
     
     budget = envelope.budget
-    out = animate % ["#budget_remaining .value", "#{financial_format budget.remaining}"]
-    out << animate % ["#budget_spent .value", "#{financial_format budget.spent.abs}"]
+    out = fade_change("#budget_remaining .value", budget.remaining)
+    out << fade_change("#budget_spent .value", budget.spent.abs)
   
-    if budget.spent_too_much
-      out << "if ($('#budget_spent .amount_over_wrap').is('.hidden')) { $('#budget_spent .amount_over_wrap').removeClass('hidden')}"
-    else
-      out << "if (!$('#budget_spent .amount_over_wrap').is('.hidden')) { $('#budget_spent .amount_over_wrap').addClass('hidden')}"
-    end
+    budget_over_wrap_selector = "#budget_spent .amount_over_wrap"
+    out << set_amount_over_visibility(budget.spent_too_much, budget_over_wrap_selector)
     
-    out << animate % ["#budget_spent .amount_over", "#{financial_format budget.amount_over}"]
+    out << fade_change("#budget_spent .amount_over", budget.amount_over)
     
-    out << animate % ["##{dom_id envelope} .remaining .value", "#{financial_format envelope.remaining.abs}"]
-    out << animate % ["##{dom_id envelope} .spent .value", "#{financial_format envelope.spent.abs}"]
+    out << fade_change("##{dom_id envelope} .remaining .value", envelope.remaining.abs)
+    out << fade_change("##{dom_id envelope} .spent .value", envelope.spent.abs)
   
-  
-    out << animate % ["##{dom_id envelope} .spent .amount_over", "#{financial_format envelope.amount_over}"]
+    out << fade_change("##{dom_id envelope} .spent .amount_over", envelope.amount_over)
     
-    if envelope.spent_too_much
-      out << "if ($('##{dom_id envelope} .spent .amount_over_wrap').is('.hidden')) { $('##{dom_id envelope} .spent .amount_over_wrap').removeClass('hidden')}"
-    else
-      out << "if (!$('##{dom_id envelope} .spent .amount_over_wrap').is('.hidden')) { $('##{dom_id envelope} .spent .amount_over_wrap').addClass('hidden')}"
-    end
+    envelope_over_wrap_selector = "##{dom_id envelope} .spent .amount_over_wrap"
+    out << set_amount_over_visibility(envelope.spent_too_much, envelope_over_wrap_selector)
     
     goal = transaction.goal
     unless goal.nil?
-      out << animate % ["##{dom_id goal} .saved .value", "#{financial_format goal.total}"]
-      out << animate % ["##{dom_id goal} .remaining .value", "#{financial_format goal.remaining}"]
+      out << fade_change("##{dom_id goal} .saved .value", goal.total)
+      out << fade_change("##{dom_id goal} .remaining .value", goal.remaining)
     end
     
     return out
@@ -72,4 +47,39 @@ module ApplicationHelper
   def link_to_unless_nil(text, object)
     link_to(text, object) unless object.nil?
   end
+  
+  private
+  
+  def set_amount_over_visibility(spent_too_much, sel)
+    if spent_too_much
+      return show_if_hidden(sel)
+    else
+      return hide_if_shown(sel)
+    end
+  end
+  
+  def show_if_hidden(sel)
+    "if ($('#{sel}').is('.hidden')) { $('#{sel}').removeClass('hidden'); }"
+  end
+  
+  def hide_if_shown(sel)
+    "if (!$('#{sel}').is('.hidden')) { $('#{sel}').addClass('hidden'); }"
+  end
+  
+  def fade_change(selector, value)
+    animate = "$('%s').animate(
+      { opacity: 0 },
+      350, function() {
+        $(this).html('%s').animate(
+          { opacity: 1 },
+          350, function() {
+            // Animation complete.
+          }
+        );
+      }
+    );"
+    
+    return animate % [selector, "#{financial_format value}"]
+  end
+  
 end
