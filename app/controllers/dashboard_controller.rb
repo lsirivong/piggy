bf8d415536d18ad579f8c7cc752f189d693fe22a
Lenny Sirivong <lsirivong@gmail.com>
2012-01-18 12:26:34 -0600

    fix logic to show new budget helper message.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,6 @@ class DashboardController < ApplicationController
     if current_user.budgets.any?
       redirect_to current_user.budgets.order("created_at DESC").first
     else
-      session[:no_budgets_found] = true
       redirect_to new_budget_path
     end
   end
