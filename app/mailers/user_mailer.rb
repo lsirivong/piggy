83b9eb7cd42c321897e24f306011cfe1464e586b
Lenny Sirivong <lsirivong@gmail.com>
2012-01-24 17:55:30 -0600

    Fix bug where activation success email referenced hardcoded url for login url.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,12 @@ class UserMailer < ActionMailer::Base
 
   def activation_needed_email(user)
     @user = user
-    # @url = "#{request.host_with_port}/users/#{user.activation_token}/activate"
     mail(:to => @user.email,
       :subject => "Welcome to Piggy")
   end
 
   def activation_success_email(user)
     @user = user
-    @url = "http://0.0.0.0:3000/login"
     mail(:to => @user.email,
       :subject => "Piggy account activation")
   end
