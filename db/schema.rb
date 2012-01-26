21ae2e6e8fe06c6aaa9d554e652cb0379a77add2
Lenny Sirivong <lsirivong@gmail.com>
2012-01-25 18:53:11 -0600

    Use a string to track form submissions. The string concats the dom_id and the time, this fixes the issue where more than one transaction couldn't be edited because their forms were generated at the same time. Also apply the before_filter on all format requests, not just js. For HTML requests, a detected double submit will redirect to root.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120125212032) do
+ActiveRecord::Schema.define(:version => 20120126002720) do
 
   create_table "budgets", :force => true do |t|
     t.date     "start_date"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20120125212032) do
     t.datetime "time"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "token"
   end
 
   create_table "transactions", :force => true do |t|
