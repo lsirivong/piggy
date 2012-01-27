7029dac9d0c224622a0b593c7e0a4281b13a0a62
Lenny Sirivong <lsirivong@gmail.com>
2012-01-27 10:24:02 -0600

    Store computed_amount of envelopes.

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -6,15 +6,23 @@ class Budget < ActiveRecord::Base
   validates :start_date, :presence => true
   validates :end_date, :presence => true
   validate :end_must_be_after_start
-  
+
   accepts_nested_attributes_for :envelopes
+
+  after_update do |budget|
+    budget.compute_envelope_amounts
+  end
   
+  after_create do |budget|
+    budget.compute_envelope_amounts
+  end
+
   def end_must_be_after_start
     if (end_date.present? && start_date.present? && (end_date <=> start_date) <= 0)
       errors.add(:end_date, "must be after start date")
     end
   end
-  
+
   def previous_budget
     user.budgets.first(:conditions => ["end_date < ?", end_date], :order => "end_date desc")
   end
@@ -34,5 +42,12 @@ class Budget < ActiveRecord::Base
   include Ledger # requires sum and amount methods
   def sum
     transactions.sum(:amount)
+  end
+
+  def compute_envelope_amounts
+    envelopes.each do |e|
+      e.compute_amount
+      e.save!
+    end
   end
 end
